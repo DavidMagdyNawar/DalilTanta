3784974e65e906501643ab42897ebc0b81299b72
david magdy <davidmagdynawar@gmail.com>
2017-10-23 15:39:26 +0200

    Replacing defaultSlider with TextSlider

--- app/src/main/java/com/daliltanta/mainscreen/HomeFragment.java
+++ app/src/main/java/com/daliltanta/mainscreen/HomeFragment.java
@@ -107,7 +107,7 @@ public class HomeFragment extends Fragment implements BaseSliderView.OnSliderCli
                 if(!imageList.equals(null))
                 {
                 for(int i = 0 ; i<imageList.size();i++){
-                    DefaultSliderView defaultSliderView = new DefaultSliderView(getContext());
+                    TextSliderView defaultSliderView = new TextSliderView(getActivity());
 
                     defaultSliderView
                             .image(imageList.get(i))
